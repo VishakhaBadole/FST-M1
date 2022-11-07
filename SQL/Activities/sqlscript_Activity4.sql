625bbf5792f7a4be45c894565ff5cadc53337a34
VishakhaBadole <105856929+VishakhaBadole@users.noreply.github.com>
2022-11-07 11:21:52 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript_Activity4.sql
@@ -0,0 +1,9 @@
+REM   Script: SQL_Activity4
+REM   Pass
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
---
+++ SQL/Activities/sqlscript_Activti1.sql
@@ -0,0 +1,13 @@
+REM   Script: SQL_Activity1
+REM   Pass
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
